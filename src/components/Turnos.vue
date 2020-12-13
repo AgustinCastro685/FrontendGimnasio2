89e744bb09c3a7803eae3f2775ee806cae16df2e
Santi <tiagoserrao@outlook.com>
2020-12-13 19:30:51 -0300

    last changes

--- src/components/Turnos.vue
+++ src/components/Turnos.vue
@@ -57,7 +57,7 @@
 
                                 </div>
 
-                                  <router-link to='/confirmacion-turno'>
+                                  
                                   <div class="form-group">
                                     <input 
                                       type="submit"
@@ -66,7 +66,7 @@
                                       value="Reservar"
                                     >
                                   </div>
-                                  </router-link>
+                                  
 
                       </form>
                     </div>
