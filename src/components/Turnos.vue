452100beaa46d4912a2a96d66231d4b6d78b676c
unknown <castroagustin2000@gmail.com>
2020-12-13 18:43:02 -0300

    Agregando funcionalidad a la reserva

--- src/components/Turnos.vue
+++ src/components/Turnos.vue
@@ -8,7 +8,6 @@
             <td>Clase</td>
             <td>Dia</td>
             <td>Horario</td>
-            <td>Anotarse</td>
           </tr>
         </thead>
         <tbody v-if="clases.length > 0">
@@ -18,17 +17,60 @@
                             <td>{{clase.dia}}</td>
                             <td>{{clase.hora}}</td>
                             
-                            <router-link to=/confirmacion-turno>
-                            <button 
-                            class="btn btn-success m-3" @click = "anotarse(clase.codClase)">Anotarse! 
-                            </button>
-                            </router-link>
-                             
-              
-              
-                            
           </tr>
         </tbody>
+
+        <!-- Botones bajo la tabla -->
+                    <!-- Boton Agregar  -->
+                    <button class="btn btn-success m-3" @click="reservar = !reservar">
+                      {{ reservar ? "No Reservar" : "Reservar" }}
+                    </button>
+                    <div v-if="reservar" class="alert alert-warning">
+                      <form novalidate autocomplete="off" @submit.prevent="enviar()">
+      
+                                  <div class="form-group">
+                                    <label for="codClase">Escriba el codigo de la clase</label>
+                                    <input 
+                                      type="number" 
+                                      id="codClase" 
+                                      class="form-control"
+                                      v-model.number="v.f.codClase.$model"
+                                      >
+                                      <div v-if="v.f.codClase.$error && v.f.codClase.$dirty" class="alert alert-danger mt-1">
+                                      <div v-if="v.f.codClase.required.$invalid">Este campo es requerido</div>
+                                        <div v-else-if="v.f.codClase.minLength.$invalid">El codigo de la clase debe tener al menos 3 caracteres</div>
+                                      </div>
+                                  </div>
+
+
+                                  <div class="form-group">
+                                    <label for="dni">Ingrese su DNI</label>
+                                    <input 
+                                      type="number" 
+                                      id="dni" 
+                                      class="form-control"
+                                      v-model="v.f.dni.$model"
+                                      >
+                                      <div v-if="v.f.dni.required.$invalid">Este campo es requerido</div>
+                                      <div v-else-if="v.f.dni.minLength.$invalid">El dni debe tener al menos 8 caracteres</div>
+                                      <div v-else-if="v.f.dni.maxLength.$invalid">El dni debe tener maximo 8 caracteres</div>
+
+                                </div>
+
+                                  
+                                  <div class="form-group">
+                                    <input 
+                                      type="submit"
+                                      :disabled="v.$invalid"
+                                      class="btn btn-success mt-4"
+                                      value="Cargar"
+                                    >
+                                    
+                                  </div>
+
+                      </form>
+                    </div>
+
         <tbody v-else-if="clases.length == 0">
           <tr>
             <td colspan="5">
@@ -44,6 +86,8 @@
 
 <script>
 import Navbar from './Navbar.vue'
+import {required ,minLength, maxLength} from '@vuelidate/validators'
+import {useVuelidate} from '@vuelidate/core'
   export default  {
     name: 'src-components-turnos',
     props: [],
@@ -56,22 +100,47 @@ import Navbar from './Navbar.vue'
         }
       this.getDatosFormAxios()
     },
+    created (){
+      const rules = {
+          f: {
+                codClase:{
+                  required,
+                  minLength: minLength(3)
+                },
+                dni:{
+                  required,
+                  minLength: minLength(8),
+                  maxLength: maxLength(8)
+                }
+                
+             }
+      }
+      const f = this.f
+      this.v = useVuelidate(rules, {f})
+    },
     data () {
       return {
+         f: {
+          codClase:'',
+          dni:'',
+          },
+        v : null,
+        reservar: false,
         url: 'http://localhost:3000/api/clases/',
+        urlAnotarse: 'http://localhost:3000/api/reservas/',
         clases:[]
-      }
+       }
     },
     methods: {
-      anotarse(id){
-        console.log("anotarse",id)
-        let usuarioConectado = JSON.parse(sessionStorage.getItem('client'))
-        console.log(usuarioConectado)
+      //anotarse(codClase){
+        //console.log("anotarse",codClase)
+        //let usuarioConectado = JSON.parse(sessionStorage.getItem('client')) 
         // hay que leer de vuex
         //this.$store.state
-        this.putUsuarioEnClase(id,usuarioConectado)
+        //console.log(codClase, usuarioConectado)
+        //this.postReservaUsuario(codClase,usuarioConectado.dni)
         
-      },
+      //},
       getDatosFormAxios(){
           this.axios(this.url)
           .then(resp => {
@@ -80,18 +149,47 @@ import Navbar from './Navbar.vue'
             })
           .catch(error => console.log('HTTP GET ERROR',error))
       },
-      putUsuarioEnClase(id,usuarioConectado) {
-            this.axios.put(this.url+id, usuarioConectado, {'content-type':'application/json'})
-            .then(resp => {
-              console.log(resp.data)
-              
-            })
-            .catch(error => console.log('HTTP PUT ERROR', error))
+      //postReservaUsuario(codClase,usuarioConectado) {
+      //      this.axios.post(this.urlAnotarse, codClase , usuarioConectado.dni, {'content-type':'application/json'})
+      //      .then(resp => {
+      //        console.log(resp.data)
+      //        
+      //      })
+      //      .catch(error => console.log('HTTP POST ERROR', error))
+      //  }
+    //},
+    sendDatosFormAxios(data) {
+      this.axios
+        .post(this.urlAnotarse, data, { "content-type": "application/json" })
+        .then(resp => {
+          console.log(resp.data)
+          this.getDatosFormAxios()
+          })
+        .catch((error) => {
+          console.log(error);
+          this.error = true;
+        })
+      },
+      enviar(){
+        this.v.$touch()
+        if(!this.v.$invalid){
+            let form = this.f
+          console.log(form)
+          this.sendDatosFormAxios(form)
+          this.resetForm()
+          this.v.$reset()
+          this.getDatosFormAxios()  
         }
-    },
+        
+      },
+      resetForm(){
+      this.v.f.codClase.$model = ''
+      this.v.f.dni.$model = ''
+      },
     computed: {
 
     }
+}
 }
 
 
