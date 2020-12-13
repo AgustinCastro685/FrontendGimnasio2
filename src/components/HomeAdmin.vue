916009bfb219db9e26c494c91b5e09f61cd3e92d
Santi <tiagoserrao@outlook.com>
2020-12-13 19:19:54 -0300

    last changes

--- src/components/HomeAdmin.vue
+++ src/components/HomeAdmin.vue
@@ -2,6 +2,24 @@
 
   <NavbarAdmin/>
 
+  <div>
+      <h1>Reservas actuales</h1>
+       <table class="table table-striped">
+       <thead>
+        <tr class="bg-dark text-white">
+            <td>Codigo de clase</td>
+            <td>DNI alumno</td>
+          </tr>
+        </thead>
+        
+          <tr class="bg-info text-white" v-for="(reserva,index) in reservas" :key="index">
+                            <td>{{reserva.codClase}}</td>
+                            <td>{{reserva.dni}}</td>            
+          </tr>
+        </table>
+  </div>    
+
+
 </template>
 
 <script>
@@ -16,14 +34,23 @@ import NavbarAdmin from './NavbarAdmin.vue'
       if (!this.$store.state.isLoggedAdmin){
             this.$router.push('/login-adm')
         }
+      this.getDatosFormAxios()
     },
     data () {
       return {
-
+        url: 'http://localhost:3000/api/reservas/',
+        reservas:[]
       }
     },
     methods: {
-
+      getDatosFormAxios(){
+          this.axios(this.url)
+          .then(resp => {
+            console.log(resp.data)
+            this.reservas = resp.data
+            })
+          .catch(error => console.log('HTTP GET ERROR',error))
+      }
     },
     computed: {
 
